0629a7fa314eeba55125f132e9ea894d4c46c484
Arockia <arockia14203@gmail.com>
2021-03-27 14:31:58 +0530

    commit

--- src/test/java/com/iloads/demo/demo/UserServiceTest.java
+++ src/test/java/com/iloads/demo/demo/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -17,7 +18,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.iloads.demo.demo.dao.UserDao;
 import com.iloads.demo.demo.domain.UserDetail;
-import com.iloads.demo.demo.service.UserService;
 import com.iloads.demo.demo.service.impl.UserServiceImpl;
 
 /**
@@ -38,27 +38,38 @@ public class UserServiceTest {
 	 * UserServiceImpl(userDao); }
 	 */
 
-	@Disabled
 	@DisplayName("Add Service Test")
 	@Test
 	public void addServiceTest() {
 		when(userDao.add(Mockito.any(UserDetail.class))).thenReturn(true);
 		UserDetail userDetail = new UserDetail(1, "kasama", 45);
 		Assertions.assertEquals(userDetail, userService.add(userDetail));
-		userDetail = new UserDetail(3, "nenuka", 43);
+		userDetail = new UserDetail(3, "nenuka", 3);
 		Assertions.assertEquals(userDetail, userService.add(userDetail));
-		userDetail = new UserDetail(2, "nenukaf", 3);
-		Assertions.assertEquals(userDetail, userService.add(userDetail));
+		/*userDetail = new UserDetail(2, "nenukaf", 3);
+		Assertions.assertEquals(userDetail, userService.add(userDetail));*/
+		System.out.println("arockia");
+		Assertions.assertDoesNotThrow(new Executable() {
+			@Override
+			public void execute() throws Throwable {
+				UserDetail userDetail = new UserDetail(2, "nenukaf", 3);
+				System.out.println("sdfdskl;");
+				userService.add(userDetail);
+				throw new Exception("somethind went wrong");
+			}
+		});
+		
+//		Assertions.assert
 	}
 
-	/*
-	 * @Test public void addServiceTests() { UserDetail userDetail = new
-	 * UserDetail(1, "kasama", 55);
-	 * when(userDao.add(Mockito.any(UserDetail.class))).thenReturn(true);
-	 * 
-	 * Assertions.assertEquals(userDetail, userService.add(userDetail)); userDetail
-	 * = new UserDetail(3, "nenuka", 23); Assertions.assertEquals(userDetail,
-	 * userService.add(userDetail)); userDetail = new UserDetail(2, "nenukaf", 43);
-	 * Assertions.assertEquals(userDetail, userService.add(userDetail)); }
-	 */
+	
+	  @Test public void addServiceTests() { UserDetail userDetail = new
+	  UserDetail(1, "kasama", 55);
+	  when(userDao.add(Mockito.any(UserDetail.class))).thenReturn(true);
+	  
+	  Assertions.assertEquals(userDetail, userService.add(userDetail)); userDetail
+	  = new UserDetail(3, "nenuka", 23); Assertions.assertEquals(userDetail,
+	  userService.add(userDetail)); userDetail = new UserDetail(2, "nenukaf", 43);
+	  Assertions.assertEquals(userDetail, userService.add(userDetail)); }
+	 
 }
